8c1dccea59d7f500302363b9c5d3d8bce242418e
Tomi Belan <belan@google.com>
2014-04-16 22:41:18 +0200

    more complete models.

--- jjp/models.py
+++ jjp/models.py
@@ -7,33 +7,41 @@ metadata = MetaData()
 
 Users = Table('users', metadata,
     Column('id', Integer, primary_key=True),
-    Column('username', String(255), nullable=False, index=True),
+    Column('username', String(255), nullable=False, index=True, unique=True),
 )
 
-Reviews = Table('reviews', metadata,
+Issues = Table('issues', metadata,
     Column('id', Integer, primary_key=True),
     Column('title', UnicodeText, nullable=False),
-    Column('upstream', String(255), nullable=False),   # TODO: remote url?
-    Column('branch', String(255), nullable=False),   # TODO: remote url.
+    Column('upstream_branch', String(255), nullable=False),
+    Column('upstream_url', String(255), nullable=False),
+    Column('upstream_latesthash', String(255), nullable=False),
+    Column('topic_branch', String(255), nullable=False),
+    Column('topic_url', String(255), nullable=False),
+    Column('topic_latesthash', String(255), nullable=False),
     Column('status', Integer, nullable=False),
 )
+Issues.STATUS_OPEN = 0
+Issues.STATUS_SUBMITTED = 1
+Issues.STATUS_ABANDONED = 2
 
 Versions = Table('versions', metadata,
-    Column('review_id', Integer, ForeignKey('reviews.id'), index=True, primary_key=True, autoincrement=False),
+    Column('issue_id', Integer, ForeignKey('issues.id'), index=True, primary_key=True, autoincrement=False),
     Column('version_num', Integer, nullable=False, primary_key=True, autoincrement=False),
-    Column('upstream_hash', String(255), nullable=False),
-    Column('branch_hash', String(255), nullable=False),
+    Column('base_hash', String(255), nullable=False),
+    Column('topic_hash', String(255), nullable=False),
 )
 
 Messages = Table('messages', metadata,
     Column('id', Integer, nullable=False, primary_key=True),
-    Column('review_id', Integer, ForeignKey('reviews.id'), nullable=False, index=True),
+    Column('issue_id', Integer, ForeignKey('issues.id'), nullable=False, index=True),
     Column('author_id', Integer, ForeignKey('users.id'), nullable=False),
+    Column('timestamp', Integer, nullable=False, index=True, default=func.now()),
 )
 
 Threads = Table('threads', metadata,
     Column('id', Integer, nullable=False, primary_key=True),
-    Column('review_id', Integer, ForeignKey('reviews.id'), nullable=False, index=True),
+    Column('issue_id', Integer, ForeignKey('issues.id'), nullable=False, index=True),
     Column('diff_from', String(255)),
     Column('diff_to', String(255)),
     Column('diff_side', Integer),
@@ -43,11 +51,9 @@ Threads = Table('threads', metadata,
 )
 
 Comments = Table('comments', metadata,
-    Column('id', Integer, nullable=False, primary_key=True),
-    Column('message_id', Integer, ForeignKey('messages.id'), nullable=False, index=True),
-    Column('thread_id', Integer, ForeignKey('threads.id'), nullable=False, index=True),
+    Column('message_id', Integer, ForeignKey('messages.id'), primary_key=True),
+    Column('thread_id', Integer, ForeignKey('threads.id'), primary_key=True),
     Column('body', UnicodeText, nullable=False),
-    Column('timestamp', Integer, nullable=False, index=True, default=func.now()),
 )
 
 
